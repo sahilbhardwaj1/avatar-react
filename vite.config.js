1a783254d977cd8e844628241b45e98bb2aab6d9
GanpatJangra <ganpatjangra@gmail.com>
2025-09-01 23:23:25 +0530

    chore: Remove unused configuration options from Vite setup

--- vite.config.js
+++ vite.config.js
@@ -4,22 +4,4 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  define: {
-    global: 'globalThis',
-  },
-  build: {
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          vendor: ['react', 'react-dom'],
-          three: ['three', '@react-three/fiber', '@react-three/drei']
-        }
-      }
-    }
-  },
-  server: {
-    headers: {
-      'Content-Type': 'application/javascript'
-    }
-  }
 })
